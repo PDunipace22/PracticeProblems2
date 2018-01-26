a3cb2e2380b9b85c0c9cb8b99b96548331d1c89f
PDunipace22 <peterdunipace@gmail.com>
2018-01-26 10:36:32 -0500

    added methods

--- PracticeProblems2/Program.cs
+++ PracticeProblems2/Program.cs
@@ -104,16 +104,136 @@ namespace PracticeProblems2
             //        Console.WriteLine(numbers[i]);
             //    }
 
-            Console.WriteLine("How far would you like to add?");
-            int input = int.Parse(Console.ReadLine());
-            int[] number = new int[input];
-            int numSum = 0;
-            for (int i = 0; i < number.Length; i++)
-            {
-                numSum = (numSum + (i + 1));
-            }
-            Console.WriteLine(numSum);
+            //Console.WriteLine("How far would you like to add?");
+            //int input = int.Parse(Console.ReadLine());
+            //int[] number = new int[input];
+            //int numSum = 0;
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    numSum = (numSum + (i + 1));
+            //}
+            //Console.WriteLine(numSum);
 
+            //int[] numberArray = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //int minusNumber = 4;
+            //int sum = 0;
+            //for (int i = 0; i < numberArray.Length; i++)
+            //{
+            //    Console.WriteLine(numberArray[i]);
+            //}
+            //for (int i = 0; i < numberArray.Length; i++)
+            //{
+            //    int result = minusNumber;
+            //    sum = sum + numberArray[i];
+            //    Console.WriteLine(sum);
+            //}
+            //Console.WriteLine(sum - minusNumber);
+
+            //int[] userNumbers = new int[10];
+            //sum = 0;
+            //for (int i = 0; i < userNumbers.Length; i++)
+            //{
+            //    Console.WriteLine("Please enter a whole number for index " + i);
+            //    userNumbers[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < userNumbers.Length; i++)
+            //{
+            //    sum += userNumbers[i];
+            //}
+            //Console.WriteLine("The sum of your numbers is " + sum);
+            //if (sum % 2 == 0)
+            //{
+            //    Console.WriteLine("The sum is even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The sum is odd");
+            //}
+
+            //int arrayLength;
+            //double sum = 0;
+            //double average;
+            //Console.WriteLine("Enter the array length");
+            //arrayLength = int.Parse(Console.ReadLine());
+            //int[] myNumbers = new int[arrayLength];
+            //Console.WriteLine("Enter the " + arrayLength + "number");
+            //for (int i = 0; i < myNumbers.Length; i++)
+            //{
+
+            //    myNumbers[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < myNumbers.Length; i++)
+            //{
+            //    sum = sum + myNumbers[i];
+            //}
+            //Console.WriteLine("Sum of " + arrayLength + " numbers is " + sum);
+            //average = sum / myNumbers.Length;
+            //Console.WriteLine("Average of " + arrayLength + " numbers is " + average);
+
+            //Day 4
+            //Problem 1 
+            //int greaterThan = 0;
+            //Console.WriteLine("The greatest number is" + Numbers(greaterThan));
+            //Probmem 2
+            //int[] num = new int[2];
+            //Console.WriteLine("The numbers are equal is " + AreEqual(num));
+            //Problem 3
+            string[] favFoods = new string[3];
+            Console.WriteLine("My favorite foods are " + Foods(favFoods));
+        }
+
+        //static int Numbers(int greater)
+        //{
+        //    int[] nArray = new int[2];
+        //    for (int i = 0; i < nArray.Length; i++)
+        //    {
+        //        Console.WriteLine("Please enter a whole number for value " + i);
+        //        nArray[i] = int.Parse(Console.ReadLine());
+        //    }
+        //    if (nArray[0]> nArray[1])
+        //    {
+        //        greater = nArray[0];
+        //    }
+        //    else
+        //    {
+        //        greater = nArray[1];
+        //    }
+        //    return greater;
+        //}
+
+        //static string AreEqual(int[] twoNum)
+        //{
+        //    for(int i = 0; i < twoNum.Length; i++)
+        //    {
+        //        Console.WriteLine("Please enter a number");
+        //        twoNum[i] = int.Parse(Console.ReadLine());
+        //    }
+        //    string isEqual;
+        //    if (twoNum[0] == twoNum[1])
+        //    {
+        //        isEqual = "True";
+        //        return isEqual;
+        //    }
+        //    else
+        //    {
+        //        isEqual = "False";
+        //        return isEqual;
+        //    }
+        //}
+
+        static string Foods(string food)
+        {
+            for (int i = 0; i < food.Length; i++)
+            {
+                Console.WriteLine("What is your favorite food?");
+                food = (Console.ReadLine());
             }
-        } 
+            for (int i = 0; i < food.Length; i++)
+            {
+                Console.WriteLine(food + i);
+            }
+            return food;
+        }
     }
+}
+
